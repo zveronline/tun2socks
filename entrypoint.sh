9555b548789a0ef2195748ec3e67e53b725aa6d9
Артем <mail@zveronline.ru>
2024-09-06 19:13:40 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,5 @@ ifconfig tun0 up
 ip route del default
 ip route add default via 198.18.0.1 dev tun0 metric 1
 ip route add default via 10.2.0.1 dev eth0 metric 100
-ip route add 10.2.0.0/16 via 10.2.0.1 dev eth0
+ip route add 10.3.0.0/16 via 10.2.0.1 dev eth0
 tun2socks -device tun0 -proxy socks5://10.2.0.101:1080 -interface eth0
